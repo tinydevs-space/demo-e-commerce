08b5563d8ff8465643a78bfaf8f8f2c4c84330f8
BlackMerchant <zafree0019@gmail.com>
2025-12-29 23:09:21 +0800

    Fix deployment config for renamed repository

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // ✅ import router
+import { BrowserRouter } from "react-router-dom"; // ✅ import router
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/demo-e-commerce">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
